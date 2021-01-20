6991ac565397d0f4a982bd9ec60cc694f9f68eca
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-20 13:17:18 -0500

    lecture work-in-progress

--- middleware-example/src/index.js
+++ middleware-example/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 
@@ -12,8 +12,8 @@ import './styles.css';
 
 import logger from 'redux-logger';
 
-
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 function App() {
   return (
