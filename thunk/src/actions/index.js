7e7e29ea44f84ddcff8532109972208c163fb7a4
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-20 13:39:18 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,19 +1,11 @@
 
-export getQuote = ()=> {
+export getQuote = () => {
     //dispatch FETCH_QUOTE_STATE
     //do axios call
     //dispatch FETCH_QUOTE_START
     //dispatch FETCH_QUOTE_FAIL
 }
 
-export const startFetch = () => {
-    return({type: "FETCH_QUOTE_START"});
-}
-
-export const fetchSuccess = (quote) => {
-    return({type: "FETCH_QUOTE_START", payload: quote});
-}
-
-export const fetchFail = (err) => {
-    return({type: "FETCH_QUOTE_FAIL", payload: err});
-}
+// export const startFetch = () => {
+//     return({type: "FETCH_QUOTE_START"});
+// }
