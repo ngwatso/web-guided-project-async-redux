8f40d1633ac7beb675a1ff6002620700c6dbc747
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-20 13:33:03 -0500

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -2,3 +2,11 @@
 export const startFetch = () => {
     return({type: "FETCH_QUOTE_START"});
 }
+
+export const fetchSuccess = (quote) => {
+    return({type: "FETCH_QUOTE_START", payload: quote});
+}
+
+export const fetchFail = (err) => {
+    return({type: "FETCH_QUOTE_START", payload: err});
+}
