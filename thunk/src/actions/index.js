bb8ae0f2d973bedb922bb6bf9ba91edebebd4b14
kirkby <kirkby@neighbor.com>
2021-02-01 18:54:06 -0700

    progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -2,6 +2,8 @@ export const START_FETCHING_DATA = 'START_FETCHING_DATA';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
 
+import axios from 'axios';
+
 export const getQuote = () => {
   return {
     type: START_FETCHING_DATA
